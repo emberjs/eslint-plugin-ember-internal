53672fa912d2affdc857c17335c802e0c7ebca29
Jonathan Haines <jonno.haines@gmail.com>
2020-10-02 09:43:46 +1000

    Fix lint with 'eslint . --fix'

--- tests/lib/rules/require-yuidoc-code-block-type.js
+++ tests/lib/rules/require-yuidoc-code-block-type.js
@@ -7,7 +7,6 @@
 var rule = require('../../../lib/rules/require-yuidoc-code-block-type');
 var RuleTester = require('eslint').RuleTester;
 
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
@@ -15,28 +14,28 @@ var RuleTester = require('eslint').RuleTester;
 var ruleTester = new RuleTester();
 
 ruleTester.run('require-yuidoc-code-block-type', rule, {
-    valid: [
-        '/*\nDummy method.\n```js\ntest\n```\n\n@method foo\n@return {String}\n*/',
-        '/**\n@module ember\n@submodule ember-application\n*/\n'
-    ],
+  valid: [
+    '/*\nDummy method.\n```js\ntest\n```\n\n@method foo\n@return {String}\n*/',
+    '/**\n@module ember\n@submodule ember-application\n*/\n',
+  ],
 
-    invalid: [
-        {
-            code: '/**\nDummy method.\n```js\ntest1\n```\n\nhello\n\n```\ntest2\n```\nhello2\n\n```\ntest3\n```\n\n@method foo\n@return {String}\n*/',
-            errors: [
-                { message: 'Code blocks require a type, like `javascript`, for doc comments.' },
-                { message: 'Code blocks require a type, like `javascript`, for doc comments.' }
-            ]
-        }, {
-            code: '/**\nDummy method.\n```app/users/route.js\ntest1\n```\n\nhello\n\n```\ntest2\n```\nhello2\n\n@method foo\n@return {String}\n*/',
-            errors: [
-                { message: 'Code blocks require a type, like `javascript`, for doc comments.' }
-            ]
-        }, {
-            code: '/**\nDummy method.\n```\ntest\n```\n\n@method foo\n@return {String}\n*/',
-            errors: [
-                { message: 'Code blocks require a type, like `javascript`, for doc comments.' }
-            ]
-        }
-    ]
+  invalid: [
+    {
+      code:
+        '/**\nDummy method.\n```js\ntest1\n```\n\nhello\n\n```\ntest2\n```\nhello2\n\n```\ntest3\n```\n\n@method foo\n@return {String}\n*/',
+      errors: [
+        { message: 'Code blocks require a type, like `javascript`, for doc comments.' },
+        { message: 'Code blocks require a type, like `javascript`, for doc comments.' },
+      ],
+    },
+    {
+      code:
+        '/**\nDummy method.\n```app/users/route.js\ntest1\n```\n\nhello\n\n```\ntest2\n```\nhello2\n\n@method foo\n@return {String}\n*/',
+      errors: [{ message: 'Code blocks require a type, like `javascript`, for doc comments.' }],
+    },
+    {
+      code: '/**\nDummy method.\n```\ntest\n```\n\n@method foo\n@return {String}\n*/',
+      errors: [{ message: 'Code blocks require a type, like `javascript`, for doc comments.' }],
+    },
+  ],
 });
